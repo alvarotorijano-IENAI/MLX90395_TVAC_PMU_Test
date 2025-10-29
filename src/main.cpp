342a0cc9f6fde8fab5e35dfc429f67c8be52d01c
Alvaro Torijano <alvaro.torijano@ienai.space>
2025-10-29 11:53:40 +0100

    temperature working

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,72 @@
 #include <Arduino.h>
+#include "Adafruit_MLX90395.h"
 
+Adafruit_MLX90395 sensor = Adafruit_MLX90395();
 
-const int DELAY_LED = 500;
-const int LED_PIN_IENAI = 41;
+void setup(void)
+{
+  Serial.begin(115200);
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED_PIN_IENAI, OUTPUT);
-  // put your setup code here, to run once:
+  /* Wait for serial on USB platforms. */
+  while (!Serial) {
+      delay(10);
+  }
+
+  Serial.println("Starting Adafruit MLX90395 Demo");
+  
+  if (! sensor.begin_I2C()) {          // hardware I2C mode, can pass in address & alt Wire
+    Serial.println("No sensor found ... check your wiring?");
+    while (1) { delay(10); }
+  }
+  Serial.print("Found a MLX90395 sensor with unique id 0x");
+  Serial.print(sensor.uniqueID[0], HEX);
+  Serial.print(sensor.uniqueID[1], HEX);
+  Serial.println(sensor.uniqueID[2], HEX);
+
+  sensor.setOSR(MLX90395_OSR_8);
+  Serial.print("OSR set to: ");
+  switch (sensor.getOSR()) {
+    case MLX90395_OSR_1: Serial.println("1 x"); break;
+    case MLX90395_OSR_2: Serial.println("2 x"); break;
+    case MLX90395_OSR_4: Serial.println("4 x"); break;
+    case MLX90395_OSR_8: Serial.println("8 x"); break;
+  }
+
+  sensor.setResolution(MLX90395_RES_19);
+  Serial.print("Gain: "); Serial.println(sensor.getGain());
 }
 
-void loop() {
-  Serial.println("IENAI");
-  digitalWrite(LED_PIN_IENAI, HIGH);
-  delay(DELAY_LED);
-  digitalWrite(LED_PIN_IENAI, LOW);
-  delay(DELAY_LED);
-  // put your main code here, to run repeatedly:
+void loop(void) {
+  /* Get a new sensor event, normalized to uTesla */
+  sensors_event_t event; 
+  sensor.getEvent(&event);
+  
+  /* Display the results (magnetic field is measured in uTesla) */
+  Serial.print("X: "); Serial.print(event.magnetic.x);
+  Serial.print(" \tY: "); Serial.print(event.magnetic.y); 
+  Serial.print(" \tZ: "); Serial.print(event.magnetic.z); 
+  Serial.println(" uTesla ");
+
+  /* Read and display temperature using the new function */
+  float temperature = sensor.readTemperature();
+  if (!isnan(temperature)) {
+    Serial.print("Temperatura: ");
+    Serial.print(temperature);
+    Serial.println(" °C");
+  } else {
+    Serial.println("Error al leer la temperatura");
+  }
+
+  /* Alternative: Read magnetic field and temperature together */
+  float x, y, z, temp;
+  if (sensor.readDataWithTemp(&x, &y, &z, &temp)) {
+    Serial.print("Lectura combinada - X: "); Serial.print(x);
+    Serial.print(" Y: "); Serial.print(y);
+    Serial.print(" Z: "); Serial.print(z);
+    Serial.print(" Temp: "); Serial.print(temp);
+    Serial.println(" °C");
+  }
+
+  Serial.println("---");
+  delay(500);
 }
